fad57dc02ed710c44383812f2516b8fa6ce0fa20
unknown <ibr.s.ahmat@gmail.com>
2022-07-20 22:59:08 +0300

    Fix linters errors

--- decode.rb
+++ decode.rb
@@ -7,29 +7,24 @@ def decode_char(char)
     '0' => '-----', '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-', '5' => '.....',
     '6' => '-....', '7' => '--...', '8' => '---..', '9' => '----.'
   }
-
   morse_code.key(char)
 end
 # puts decode_char('-..')
 
 def decode_word(char)
   word = ''
-
   char.split.each do |c|
     word += decode_char(c).to_s
   end
-
   word
 end
 # puts decode_word("-- ....")
 
 def decode(str)
   decoded_msg = ''
-
   str.split('  ').each do |word|
     decoded_msg += "#{decode_word(word)} "
   end
-  
   decoded_msg.upcase
 end
 
